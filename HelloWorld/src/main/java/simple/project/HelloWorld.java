7998be16fa407dccc456edc611b58fab920a1a81
RAKHI <RAKHI@DESKTOP-6LDC70F>
2023-11-29 22:51:46 -0800

    commit again

--- HelloWorld/src/main/java/simple/project/HelloWorld.java
+++ HelloWorld/src/main/java/simple/project/HelloWorld.java
@@ -2,9 +2,11 @@ package simple.project;
 
 public class HelloWorld {
 
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-		System.out.println("hello world");
+		System.out.println("helo World");
+		
+
 	}
 
 	
